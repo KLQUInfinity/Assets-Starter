4185b251344362696c17ef2c958c5a9b728d22b5
ahmed bendary <31450453+KLQUInfinity@users.noreply.github.com>
2019-11-04 09:38:20 +0200

    Bendary
    Init
    end the tool

---
+++ Assets/AssetsStarter/Scripts/Creator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Creator : MonoBehaviour
+{
+    private static string[] folderNames = { "_AppAssets", "_Scenes", "Scripts", "Prefabs", "Sprites", "Materials", "Sounds", "Animations" };
+
+    [MenuItem("Tools/Create Main Assets Folder", false, 0)]
+    private static void CreateAssets()
+    {
+        // Create the MainFolder
+        if (!AssetDatabase.IsValidFolder("Assets/" + folderNames[0]))
+        {
+            AssetDatabase.CreateFolder("Assets", folderNames[0]);
+        }
+
+        // Create SubFolder
+        for (int i = 1; i < folderNames.Length; i++)
+        {
+            if (!AssetDatabase.IsValidFolder("Assets/" + folderNames[0] + "/" + folderNames[i]))
+            {
+                if (folderNames[i].Equals("_Scenes") && AssetDatabase.IsValidFolder("Assets/Scenes"))
+                {
+                    AssetDatabase.MoveAsset("Assets/Scenes", "Assets/" + folderNames[0] + "/" + folderNames[i]);
+                }
+                else
+                {
+                    AssetDatabase.CreateFolder("Assets/" + folderNames[0], folderNames[i]);
+                }
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
